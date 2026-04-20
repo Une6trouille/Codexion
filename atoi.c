82f019e55501309b34b5a495931eb97d26333421
Nathan Di Tullio <ndi-tull@z4r2p5.42lyon.fr>
2026-04-20 03:35:37 +0200

    feat(parsing): Adding Atoi
    Adds the `ft_atoi` function required to store arguments passed during program execution

---
+++ atoi.c
@@ -0,0 +1,16 @@
+#include <codexion.h>
+
+int ft_atoi(char *str)
+{
+    int result;
+    int i;
+
+    i = 0;
+    result = 0;
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        result = (result * 10) + (str[i] - '0');
+        i++;
+    }
+    return (result);
+}
