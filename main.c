69498a1a65a9c1df8fd620290736e25407c44797
Nathan <nathanditullio01@gmail.com>
2026-04-21 05:20:25 +0200

    feat: Add new functions in main.c
    Added the `free_all` function, which will be used to free all `malloc` allocations made during code execution, as well as mutexes and conditions. Also added `join_thread`, which allows threads to continue executing before the end of the main function.

--- main.c
+++ main.c
@@ -1,9 +1,44 @@
 #include "codexion.h"
 
+static void	join_threads(pthread_t *thread, pthread_t *monitor,
+		t_shared_data *shared_data)
+{
+	int	i;
+
+	i = 0;
+	while (i < shared_data->args.nb_coders)
+	{
+		pthread_join(thread[i], NULL);
+		i++;
+	}
+	pthread_join(*monitor, NULL);
+}
+
+void	free_all(t_shared_data *shared_data, t_coder *coders,
+		pthread_t *threads)
+{
+	int	i;
+
+	i = 0;
+	while (i < shared_data->args.nb_coders)
+	{
+		pthread_mutex_destroy(&shared_data->dongle[i].mutex);
+		pthread_cond_destroy(&shared_data->dongle[i].condition);
+		i++;
+	}
+	pthread_mutex_destroy(&shared_data->log_mutex);
+	free(shared_data->dongle);
+	free(coders);
+	free(threads);
+	free(shared_data);
+}
+
 int	main(int argc, char **argv)
 {
-	t_shared_data *shared_data;
-	t_coder *coders;
+	t_shared_data	*shared_data;
+	t_coder			*coders;
+	pthread_t		*threads;
+	pthread_t		monitor;
 
 	shared_data = malloc(sizeof(t_shared_data));
 	if (!parse_args(argc, argv, &shared_data->args))
@@ -13,7 +48,10 @@ int	main(int argc, char **argv)
 	}
 	init_shared_data(shared_data);
 	coders = malloc(sizeof(t_coder) * shared_data->args.nb_coders);
+	threads = malloc(sizeof(pthread_t) * shared_data->args.nb_coders);
 	init_coders(shared_data, coders);
-
+	create_thread(threads, coders, &monitor, shared_data);
+	join_threads(threads, &monitor, shared_data);
+	free_all(shared_data, coders, threads);
 	return (0);
 }
