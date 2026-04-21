38d2379bf78d47e0e4b77ebfcbc2477dbb4bedcf
Nathan <nathanditullio01@gmail.com>
2026-04-21 03:18:37 +0200

    feat: Add multiples init functions
    Added `init_dongles`, which initializes the variables in the dongle structure; the same applies to `init_coders` and `init_shared_data`

---
+++ init.c
@@ -0,0 +1,43 @@
+#include "codexion.h"
+
+void	init_dongles(t_shared_data *shared_data)
+{
+	int	i;
+
+	i = 0;
+	shared_data->dongle = malloc(sizeof(t_dongle)
+			* shared_data->args.nb_coders);
+	while (i < shared_data->args.nb_coders)
+	{
+		shared_data->dongle[i].free = 1;
+		shared_data->dongle[i].nb_in_queue = 0;
+		pthread_cond_init(&shared_data->dongle[i].condition, NULL);
+		pthread_mutex_init(&shared_data->dongle[i].mutex, NULL);
+		shared_data->dongle[i].release_time = get_time_ms();
+		shared_data->dongle[i].queue = NULL;
+		i++;
+	}
+}
+
+void	init_coders(t_shared_data *shared_data, t_coder *coders)
+{
+	int	i;
+
+	i = 0;
+	while (i < shared_data->args.nb_coders)
+	{
+		coders[i].dongle_held = 0;
+		coders[i].id = i + 1;
+		coders[i].nb_compile = 0;
+		coders[i].timestamp = 0;
+		coders[i].t_shared_data = shared_data;
+		i++;
+	}
+}
+
+void	init_shared_data(t_shared_data *shared_data)
+{
+	shared_data->simulation_over = 0;
+	pthread_mutex_init(&shared_data->log_mutex, NULL);
+	init_dongles(shared_data);
+}
